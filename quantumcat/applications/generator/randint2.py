50e9070313f88b96aff13534ef6e54b3be15bf53
swarupa29 <swarupav01@gmail.com>
2021-06-19 11:36:47 +0530

    Added new version of qrng

--- quantumcat/applications/generator/randint2.py
+++ quantumcat/applications/generator/randint2.py
@@ -19,15 +19,16 @@ import math
 import numpy as np
 from scipy.linalg import hadamard
 
-class RandInt:
+class RandInt2:
     def __init__(self, range, output_type=constants.DECIMAL):
-        super(RandInt, self).__init__()
+        super(RandInt2, self).__init__()
         self.upper_limit = range[1]
         self.lower_limit = range[0]
         self.num_qubits = self.get_num_bits()
         self.output_type = output_type
         self.provider = providers.DEFAULT_PROVIDER
         self.unitary_matrix = None
+        self.hadamard_matrix= hadamard(2**self.num_qubits)
         self.qc = None
 
     def get_num_bits(self):
@@ -39,8 +40,8 @@ class RandInt:
         lst.extend(list((range(self.upper_limit + 1, 2 ** self.num_qubits))))
         return lst
 
-    def make_matrix(self, lst):
-        H=hadamard(2**self.num_qubits)
+    def make_final_matrix(self, lst):
+        H = self.hadamard_matrix
         for i in lst:
             H[i] = np.zeros(1)
         return H
@@ -51,22 +52,33 @@ class RandInt:
         return Q
 
     def unitary_matrix_gen(self):
+        #making the unitary inverse hadamard matrix
+        hadamard_unitary = self.gs(self , self.hadamard_matrix)
+        unitary_had_inv = np.linalg.inv(hadamard_unitary)
+        
+
+        #making the unitary final matrix
         lst = self.zero_rows()
-        matrix = self.make_matrix(lst)
-        self.unitary_matrix = self.gs(self, matrix)
+        matrix = self.make_final_matrix(lst)
+        final_matrix = self.gs(self, matrix)
+
+        #making the unitary matrix to use in circuit circuit
+        self.unitary_matrix = np.matmul(final_matrix,unitary_had_inv)
 
     def make_circuit(self):
         self.unitary_matrix_gen()
         qubits = list(range(0, self.num_qubits))
         qc = QCircuit(self.num_qubits)
         self.qc = qc
+        for i in range(self.num_qubits):
+            qc.h_gate(i)
         qc.unitary(self.unitary_matrix, qubits)
         qc.measure_all()
 
     def execute(self, provider=providers.DEFAULT_PROVIDER,
                 simulator_name=constants.DEFAULT_SIMULATOR, api=None, device=None):
         self.make_circuit()
-        counts = self.qc.execute(provider=provider, repetitions=1,
+        counts = self.qc.execute(provider=provider, repetitions=8192,
                                  simulator_name=simulator_name, api=api, device=device)
 
         random_number = list(counts.keys())[0]
@@ -74,7 +86,7 @@ class RandInt:
             if provider == providers.GOOGLE_PROVIDER:
                 random_number = random_number[::-1]
             random_number = helper.binary_to_decimal(random_number)
-        return random_number
+        return counts
 
     def draw_random_number_circuit(self, provider=providers.DEFAULT_PROVIDER):
         self.make_circuit()
